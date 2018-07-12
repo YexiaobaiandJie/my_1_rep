43f97acf20f72ba0ec35d5921b1a2879ee4d7e61
helijie <1124924603@qq.com>
2018-07-12 18:03:19 +0800

    first page part

---
+++ vue_fox/src/components/Layout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <div class="header">幸运硬币</div>
+      <div class="content">
+          <keep-alive>
+          <router-view></router-view>
+          </keep-alive>
+      </div>
+      <div class="footer">footer</div>
+  </div>
+</template>
+
+<script>
+export default {
+ // name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.body{
+    background:#f1f1f1;
+}
+.header{
+    width:100%;
+    height:50px;
+    background-color: #ff4500;
+    text-align: center;
+    line-height: 50px;
+    color:#fff;
+}
+.footer{
+    width:100%;
+    height:50px;
+    background-color:#cfcfcf;
+    text-align: center;
+    line-height:50px;;
+}
+  
+</style>
---
+++ vue_fox/src/pages/index.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="content2">
+      <!--
+       <button class="row-action">
+          <image class="row-action-image" mode="aspectFit" src="../assets/index-records.png"></image>
+          
+          <div class="row-action-content">
+            <div class="row-action-title">收到的硬币</div>
+            <div class="row-action-text">来自朋友的馈赠</div>
+          </div>
+        </button>
+        <button class="row-action" >
+          <image class="row-action-image" mode="aspectFit" src="../assets/index-gift.png"></image>
+          
+          <image class="row-action-image" src="../asserts/index-gift.png"/>
+          <div class="row-action-content">
+            <div class="row-action-title">送出的硬币</div>
+            <div class="row-action-text">给朋友的祝福</div>
+          </div>
+        </button>
+        <div class="form">
+            <div >
+             <button class="form-row">我要发幸运硬币</button>
+            </div>
+            <div >
+             <button class="form-row" >我的余额账户</button>
+            </div>
+        </div> 
+        -->
+        
+          <button class="btn1">
+              <image class="imgc" src="./assets/logo.png"/>
+              <div class="zm">
+                  <div class="zm1">收到的硬币</div>
+                  <div class="zm2">来自朋友的馈赠</div>
+              </div>
+          </button>   
+          <br/>
+          <button class="btn1">
+              <image class="imgc" src="./assets/logo.png"/>
+              <div class="zm">
+                  <div class="zm1">送出的硬币</div>
+                  <div class="zm2">给朋友的祝福</div>
+              </div>
+          </button> 
+        
+
+        
+          <div >
+             <button class="btn2">我要发幸运硬币</button>
+            </div>
+            <div >
+             <button class="btn3" >我的余额账户</button>
+            </div>
+        
+
+        
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+ // name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.btn1{
+    background-color:#fff;
+    margin:20px;
+    border:none;
+    width:300px;
+    height:100px;
+    margin-top:5px;
+    border-radius: 10px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+}
+.btn2{
+    width:300px;
+    margin:5px;
+    height:30px;
+    background-color:#ffcc00;
+    border:none;
+    color:#ff6600;
+    border-radius: 8px;
+}
+.btn3{
+    width:300px;
+    margin:5px;
+    height:30px;
+    border:1px solid#ff4500;
+    color:#ff4500;
+    border-radius: 8px;
+}
+.imgc{
+width:50px;
+height:50px;
+}
+.zm{
+    width:200px;
+    height:100px;
+}
+.zm1{
+    font-size:20px;
+    margin:3px;
+}
+.zm2{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.content2{
+    text-align: center;
+    margin-top:30px;
+}
+.row-action {
+    display: flex;
+    /*align-items: center;*/
+    border-radius: 8px;
+    background: #fff;
+    padding: 10px 20px;
+    margin-bottom: 10px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+}
+    .row-action-image {
+      flex-basis: 70px;
+      height: 70px;
+      width: 70px;
+    }
+    .row-action-content {
+      padding: 10px 0;
+      margin-left: 20px;
+      text-align: left;
+      display: flex;
+      flex-direction: column;
+     /* flex: 1;*/
+    }
+    .row-action-title {
+      font-size: 20px;
+      flex: 1;
+    }
+    .row-action-text {
+      flex: 0;
+      font-size: 14px;
+    }
+    .form {
+    padding: 20px 16px;
+    }
+    .form-row{
+        width:200px;
+        height:30px;
+       
+        background: #fff;
+       /* display: flex;*/
+        align-items: center;
+        
+       /* margin-bottom: 10px;*/
+        /*padding: 10px 20px;*/
+        
+        box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+        border-radius: 8px;
+       
+    }
+  
+</style>
