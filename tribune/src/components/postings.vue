36f5ff2a0d1b8af4e4641459eb7c09d9ee7a2fe6
helijie <1124924603@qq.com>
2018-08-01 18:53:02 +0800

    postings页面布局

--- tribune/src/components/postings.vue
+++ tribune/src/components/postings.vue
@@ -1,19 +1,111 @@
 <template>
-<div>
-    <div>{{message}}</div>
-</div>
+    <div class="content">
+        <div class="posttitle">
+            <h2>论帖>></h2>
+        </div>
+        <hr />
+            <div  v-for="postitem in postitems" class="poststyle">
+                <div class="post-l">{{postitem.title}}</div>
+                <div class="post-r">{{postitem.date}}</div>
+            </div>
+            <button class="btnpublish" v-bind:class="{clickbtn:ison}" v-on:mouseover="onbtn" v-on:mouseout="gobtn">发帖</button>
+    </div>
 </template>
+
 
 <script>
 export default{
+    mounted:function(){
+        this.loadnews()
+    },
     data(){
         return{
-            message:"this is the postings components"
+            ison:false,
+            postitems:[
+                {
+                    title:"帖子的题目",
+                    date:"2018-08-01"
+                },
+                {
+                    title:"帖子的题目",
+                    date:"2018-08-01"
+                }
+            ]
+        }
+    },
+    methods:{
+        loadnews:function(){
+        this.$http.get('http://localhost:8080/news?token=5b5ff5794cf88aed639018e0&pagesize=10').then(response => {
+            console.log(response.data);
+        },response => {
+            console.log("error");
+        });
+        },
+        onbtn:function(){
+            this.ison=true
+        },
+        gobtn:function(){
+            this.ison=false
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+.content{
+    width:90%;
+    margin-left:5%;
+    height:100%;
+    
+}
+.posttitle{
+    text-align:left;
+    padding-left:10px;
+    margin-bottom:-10px;
+    margin-top:-2px;
+   
+}
+.poststyle{
+    width:100%;
+    
+}
+.post-l{
+   text-align: left;
+   height:10%;
+   width:70%;
+   float: left;
+   padding-left:1%;
+   font-size:120%;
+   padding-bottom:0.4%;
+   padding-top:0.4%;
+   
+}
+.post-r{
+   text-align:right;
+   height:10%;
+   width:27%;
+   float: left;
+   font-size:120%;
+   padding-right:15px;
+   padding-bottom: 0.4%;
+   padding-top:0.4%;
+}
+.btnpublish{
+    width:8%;
+    font-size:108%;
+    padding-top:4px;
+    padding-bottom: 4px;
+    margin-top:2%;
+    margin-right:1%;
+    float:right;
+    background-color:#555;
+    border:none;
+    color:aliceblue;
+    transition:0.7s all;
+}
+.clickbtn{
+    background-color:#ff4500;
+    transition:0.7s all;
+} 
 
 </style>
